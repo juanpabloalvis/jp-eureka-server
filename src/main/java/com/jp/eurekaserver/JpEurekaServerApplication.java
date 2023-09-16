3b0d4db6eb3314889f6db007425c76682eaf72e0
Juan Pablo Alvis C <juanpabloalvis@gmail.com>
2023-09-16 10:42:55 -0500

    Configurations for eureka server

--- src/main/java/com/jp/eurekaserver/JpEurekaServerApplication.java
+++ src/main/java/com/jp/eurekaserver/JpEurekaServerApplication.java
@@ -2,12 +2,14 @@ package com.jp.eurekaserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class JpEurekaServerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(JpEurekaServerApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(JpEurekaServerApplication.class, args);
+    }
 
 }
