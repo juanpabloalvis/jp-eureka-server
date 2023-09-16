425cb012e7ee79443ce1be8c1e40c40f356105dd
Juan Pablo Alvis C <juanpabloalvis@gmail.com>
2023-09-16 10:04:19 -0500

    Commit inicial

---
+++ src/main/java/com/jp/eurekaserver/JpEurekaServerApplication.java
@@ -0,0 +1,13 @@
+package com.jp.eurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpEurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpEurekaServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jp/eurekaserver/JpEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jp.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpEurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
